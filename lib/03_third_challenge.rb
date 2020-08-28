94085fd1647df5e1e316209c28857dcec2585e3a
Marisa You <marisa.you@gmail.com>
2020-08-28 23:48:19 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -24,10 +24,11 @@ def third_challenge
       :matriarch => {
         :name => "Lady Capulet",
         :age => "51"
-        
         },
       :heroine => {
-        #your key/value pairs here
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => []
    }
